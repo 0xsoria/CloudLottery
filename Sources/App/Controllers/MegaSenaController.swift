9ba2713abceee45eab049a2246e6774f704038c7
Gabriel Soria Souza <maczepp@Gabriels-MacBook-Pro.local>
2019-02-27 21:55:07 -0300

    Adding MegaSenaController and possibility to POST array of MegaSena.

---
+++ Sources/App/Controllers/MegaSenaController.swift
@@ -0,0 +1,97 @@
+//
+//  MegaSenaController.swift
+//  App
+//
+//  Created by Gabriel Soria Souza on 22/02/19.
+//
+
+import Vapor
+import Fluent
+
+struct MegaSenaController: RouteCollection {
+    
+    func boot(router: Router) throws {
+        
+        let megaSenaRouter = router.grouped("api", "sena")
+        megaSenaRouter.get(use: getAllHandler)
+        megaSenaRouter.post([MegaSena].self, use: createHandler)
+        megaSenaRouter.get(MegaSena.parameter, use: getHandler)
+        megaSenaRouter.put(MegaSena.parameter, use: updateHandler)
+        megaSenaRouter.delete(MegaSena.parameter, use: deleteHandler)
+        megaSenaRouter.get("search", use: searchHandler)
+        megaSenaRouter.get("first", use: getFirstHandler)
+        megaSenaRouter.get("sorted", use: sortedHandler)
+    }
+    
+    func createHandler(_ req: Request, games: [MegaSena]) throws -> Future<HTTPStatus> {
+        var arr: [EventLoopFuture<MegaSena>] = []
+        
+        for i in games {
+            arr.append(i.save(on: req))
+        }
+        return arr.flatten(on: req).transform(to: .created)
+    }
+
+    
+    func getHandler(_ req: Request) throws -> Future<MegaSena> {
+        return try req.parameters.next(MegaSena.self)
+    }
+    
+    func updateHandler(_ req: Request) throws -> Future<MegaSena> {
+        return try flatMap(
+            to: MegaSena.self,
+        req.parameters.next(MegaSena.self),
+        req.content.decode(MegaSena.self)) { game, updateGame in
+            game.numberOfTheGame = updateGame.numberOfTheGame
+            game.data_sorteio = updateGame.data_sorteio
+            game.dezena_um = updateGame.dezena_um
+            game.dezena_dois = updateGame.dezena_dois
+            game.dezena_tres = updateGame.dezena_tres
+            game.dezena_quatro = updateGame.dezena_quatro
+            game.dezena_cinco = updateGame.dezena_cinco
+            game.dezena_seis = updateGame.dezena_seis
+            game.all_numbers = updateGame.all_numbers
+            game.arrecadacao_total = updateGame.arrecadacao_total
+            game.ganhadores_sena = updateGame.ganhadores_sena
+            game.cidade = updateGame.cidade
+            game.uf = updateGame.uf
+            game.rateio_sena = updateGame.rateio_sena
+            game.ganhadores_quina = updateGame.ganhadores_quina
+            game.rateio_quina = updateGame.rateio_quina
+            game.ganhadores_quadra = updateGame.ganhadores_quadra
+            game.acumulado = updateGame.acumulado
+            game.valor_acumulado = updateGame.valor_acumulado
+            game.estimativa_premio = updateGame.estimativa_premio
+            game.acumulado_mega_da_virada = updateGame.acumulado_mega_da_virada
+            game.allNumbersString = updateGame.allNumbersString
+            return game.save(on: req)
+        }
+    }
+    
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(MegaSena.self).delete(on: req).transform(to: HTTPStatus.noContent)
+    }
+    
+    func searchHandler(_ req: Request) throws -> Future<[MegaSena]> {
+        guard let searchTerm = req.query[String.self, at: "term"] else {
+            throw Abort(.badRequest)
+        }
+        return MegaSena.query(on: req).group(.or) { or in
+            or.filter(\.allNumbersString == searchTerm)}.all()
+    }
+    
+    func getFirstHandler(_ req: Request) throws -> Future<MegaSena> {
+        return MegaSena.query(on: req).first().map(to: MegaSena.self) { sena in
+            guard let sena = sena else { throw Abort(.notFound) }
+            return sena
+        }
+    }
+    
+    func sortedHandler(_ req: Request) throws -> Future<[MegaSena]> {
+        return MegaSena.query(on: req).sort(\.data_sorteio, .ascending).all()
+    }
+    
+    func getAllHandler(_ req: Request) throws -> Future<[MegaSena]> {
+        return MegaSena.query(on: req).all()
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,77 +13,8 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
     
-    router.post("api", "sena") { req -> Future<MegaSena> in
-        return try req.content.decode(MegaSena.self)
-            .flatMap(to: MegaSena.self) { game in
-                return game.save(on: req)
-        }
-    }
-    
-    router.get("api", "sena") { req -> Future<[MegaSena]> in
-        return MegaSena.query(on: req).all()
-    }
-    
-    router.get("api", "sena", MegaSena.parameter) { req -> Future<MegaSena> in
-        return try req.parameters.next(MegaSena.self)
-    }
-    router.put("api", "sena", MegaSena.parameter) { req -> Future<MegaSena> in
-        return try flatMap(to: MegaSena.self, req.parameters.next(MegaSena.self), req.content.decode(MegaSena.self)) {
-            game, updateGame in
-            
-            game.numberOfTheGame = updateGame.numberOfTheGame
-            game.data_sorteio = updateGame.data_sorteio
-            game.dezena_um = updateGame.dezena_um
-            game.dezena_dois = updateGame.dezena_dois
-            game.dezena_tres = updateGame.dezena_tres
-            game.dezena_quatro = updateGame.dezena_quatro
-            game.dezena_cinco = updateGame.dezena_cinco
-            game.dezena_seis = updateGame.dezena_seis
-            game.all_numbers = updateGame.all_numbers
-            game.arrecadacao_total = updateGame.arrecadacao_total
-            game.ganhadores_sena = updateGame.ganhadores_sena
-            game.cidade = updateGame.cidade
-            game.uf = updateGame.uf
-            game.rateio_sena = updateGame.rateio_sena
-            game.ganhadores_quina = updateGame.ganhadores_quina
-            game.rateio_quina = updateGame.rateio_quina
-            game.ganhadores_quadra = updateGame.ganhadores_quadra
-            game.acumulado = updateGame.acumulado
-            game.valor_acumulado = updateGame.valor_acumulado
-            game.estimativa_premio = updateGame.estimativa_premio
-            game.acumulado_mega_da_virada = updateGame.acumulado_mega_da_virada
-            game.allNumbersString = updateGame.allNumbersString
-            
-            return game.save(on: req)
-        }
-    }
-    
-    router.delete("api", "sena", MegaSena.parameter) { req -> Future<HTTPStatus> in
-        return try req.parameters.next(MegaSena.self)
-                    .delete(on: req)
-                    .transform(to: HTTPStatus.noContent)
-    }
-    
-    router.get("api", "sena", "search") { req -> Future<[MegaSena]> in
-        guard let searchTerm = req.query[String.self, at: "term"] else {
-            throw Abort(.badRequest)
-        }
-        return MegaSena.query(on: req).filter(\.allNumbersString == searchTerm).all()
-    }
-    
-    router.get("api", "sena", "first") { req -> Future<MegaSena> in
-        return MegaSena.query(on: req).first().map(to: MegaSena.self) { game in
-            guard let game = game else {
-                throw Abort(.notFound)
-            }
-            return game
-        }
-    }
-    
-    router.get("api", "sena", "sorted") { req -> Future<[MegaSena]> in
-        return MegaSena.query(on: req).sort(\.data_sorteio, .ascending).all()
-    }
-    
+    let megaSenaController = MegaSenaController()
+    try router.register(collection: megaSenaController)
     
     router.post("api", "estatisticas", "megasena") { req -> Future<MegaSenaStatistics> in
         return try req.content.decode(MegaSenaStatistics.self)
