7679f2e9d306ebd332e55552d3496697694596c0
Gabriel Soria Souza <maczepp@Gabriels-MacBook-Pro.local>
2019-02-28 21:27:20 -0300

    y

---
+++ Tests/AppTests/MegaSenaTests.swift
@@ -0,0 +1,22 @@
+@testable import App
+import Vapor
+import XCTest
+import FluentPostgreSQL
+
+final class MegaSenaTests: XCTestCase {
+    
+//    func test() throws {
+//        
+//        
+//        var config = Config.default()
+//        var services = Services.default()
+//        var env = Environment.testing
+//        try App.configure(&config, &env, &services)
+//        try App.boot(app)
+//        
+//        let conn = try app.newConnection(to: .psql).wait()
+//        
+//        let sena = MegaSena(id: <#T##Int#>, numberOfTheGame: <#T##Int#>, data_sorteio: <#T##String#>, dezena_um: <#T##Int#>, dezena_dois: <#T##Int#>, dezena_tres: <#T##Int#>, dezena_quatro: <#T##Int#>, dezena_cinco: <#T##Int#>, dezena_seis: <#T##Int#>, all_numbers: <#T##[Int]#>, arrecadacao_total: <#T##String#>, ganhadores_sena: <#T##Int#>, cidade: <#T##String#>, uf: <#T##String#>, rateio_sena: <#T##String#>, ganhadores_quina: <#T##Int#>, rateio_quina: <#T##String#>, ganhadores_quadra: <#T##Int#>, rateio_quadra: <#T##String#>, acumulado: <#T##String#>, valor_acumulado: <#T##String#>, estimativa_premio: <#T##String#>, acumulado_mega_da_virada: <#T##String#>)
+//        
+//    }
+}
